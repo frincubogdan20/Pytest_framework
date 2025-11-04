a6bbdb6fd6b0a9f59f407dc7f35adbdd86b36dd2
Bogdan Frincu <frincu.bogdan20@gmail.com>
2025-11-04 15:19:58 +0200

    Add broken table booking test

--- tests/test_academy_bugs.py
+++ tests/test_academy_bugs.py
@@ -38,3 +38,21 @@ def test_article_links(page: Page) -> None:
     with page.expect_navigation():
         article_links.nth(0).click()
     assert "Why Do I Need To Use Financial Consulting Service?" in page.title()
+
+def test_academy_book_table(page: Page) -> None:
+    academy = Goto(page)
+
+    # This is a placeholder for a broken book a table test
+    academy.academy_go_to("my-bookings/")
+    page.click("input#rtb-date")
+    page.locator("[aria-label='November 15, 2025']").click()
+    page.click("input#rtb-time")
+    page.locator("[aria-label='10:00 PM']").click()
+    page.locator("select#rtb-party").select_option("4")
+    page.fill("input#rtb-name", "admin")
+    page.fill("input#rtb-email", "admin@academybugs.com")
+    page.fill("input#rtb-phone", "1234567890")
+    page.locator("div.add-message > a").click()
+    page.fill("textarea#rtb-message", "Table with a view please.")
+    page.click("button#academy-booking-submit")
+    expect(page.locator("div.rtb-confirmation-message")).to_contain_text("Thank you for your booking request")
